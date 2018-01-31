71675b6c58ec13a6bb03182f306f9fd6a3dd7edc
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-31 10:22:09 -0600

    Set up Field component and render list of fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default class PostsNew extends Component {
-  render() {
+class PostsNew extends Component {
+
+  renderField(field) {
     return (
-      <div>
-        Posts
+      <div className="form-group">
+        <label>{field.label}:</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        {/*field.input is an object that contains event handlers*/}
       </div>
     );
   }
+
+  render() {
+    return (
+      <form>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
+  }
 }
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
