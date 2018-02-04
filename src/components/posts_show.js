79275b93864fe09a30b6859279a1a05d68a34046
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-02-03 19:32:06 -0600

    Display the post with the correct id

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
-export default class PostsShow extends Component {
+class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const { post } = this.props;
+
+    if(!post) {
+      return (<div>Loading...</div>);
+    }
+
     return (
       <div>
-        Posts Show
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
+
+function mapStateToProps({ posts }, ownProps) {
+  return {
+    post: posts[ownProps.match.params.id] //ownProps === this.props in this context
+  };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
